5379839982ce0a8e45ca727b8e7333745832ab02
aengus <aengus.hearne@gmail.com>
2018-03-07 22:37:17 -0500

    Add fetch report

--- index.js
+++ index.js
@@ -23,3 +23,27 @@ let selector = {
 campaignService.get({serviceSelector: selector}, (error, result) => {
     console.log(error, result);
 })
+
+const AdwordsReport = require('node-adwords').AdwordsReport;
+ 
+let report = new AdwordsReport({/** same config as AdwordsUser above */
+    developerToken: process.env.DEVELOPER_TOKEN, //your adwords developerToken
+    userAgent: process.env.USER_AGENT, //any company name
+    clientCustomerId: process.env.CLIENT_CUSTOMER_ID, //the Adwords Account id (e.g. 123-123-123)
+    client_id: process.env.CLIENT_ID, //this is the api console client_id
+    client_secret: process.env.CLIENT_SECRET,
+    refresh_token: process.env.REFRESH_TOKEN});
+report.getReport('v201802', {
+    reportName: 'Custom Adgroup Performance Report',
+    reportType: 'CAMPAIGN_PERFORMANCE_REPORT',
+    fields: ['CampaignId', 'Impressions', 'Clicks', 'Cost'],
+    filters: [
+        {field: 'CampaignStatus', operator: 'IN', values: ['ENABLED', 'PAUSED']}
+    ],
+    dateRangeType: 'CUSTOM_DATE', //defaults to CUSTOM_DATE. startDate or endDate required for CUSTOM_DATE
+    startDate: new Date("07/10/2016"),
+    endDate: new Date(),
+    format: 'CSV' //defaults to CSV
+}, (error, report) => {
+    console.log(error, report);
+});
