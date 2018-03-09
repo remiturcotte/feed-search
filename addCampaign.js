071f141f84243d2f00c0a53b1aae51cc39751cce
aengushearne <aengus.hearne@gmail.com>
2018-03-09 14:02:56 -0500

    Use uuid and moment

--- addCampaign.js
+++ addCampaign.js
@@ -1,99 +1,117 @@
 require('dotenv').load();
+const uuid = require('uuid/v4');
+const moment = require('moment');
 const AdwordsUser = require('node-adwords').AdwordsUser;
 const AdwordsConstants = require('node-adwords').AdwordsConstants;
 
-let user = new AdwordsUser({
-    developerToken: process.env.DEVELOPER_TOKEN, //your adwords developerToken
-    userAgent: process.env.USER_AGENT, //any company name
-    clientCustomerId: process.env.CLIENT_CUSTOMER_ID, //the Adwords Account id (e.g. 123-123-123)
-    client_id: process.env.CLIENT_ID, //this is the api console client_id
-    client_secret: process.env.CLIENT_SECRET,
-    refresh_token: process.env.REFRESH_TOKEN
+const user = new AdwordsUser({
+  developerToken: process.env.DEVELOPER_TOKEN, //your adwords developerToken
+  userAgent: process.env.USER_AGENT, //any company name
+  clientCustomerId: process.env.CLIENT_CUSTOMER_ID, //the Adwords Account id (e.g. 123-123-123)
+  client_id: process.env.CLIENT_ID, //this is the api console client_id
+  client_secret: process.env.CLIENT_SECRET,
+  refresh_token: process.env.REFRESH_TOKEN
 });
 
-let campaignService = user.getService('CampaignService', 'v201802')
-let budget_service = user.getService('BudgetService', 'v201802')
+const campaignService = user.getService('CampaignService', 'v201802');
+const budget_service = user.getService('BudgetService', 'v201802');
 
 // Create a budget, which can be shared by multiple campaigns.
-let budget = {
-      'name': 'Interplanetary budget 3456789',// % uuid.uuid4(),
-      'amount': {
-          'microAmount': '50000000'
+const budget = {
+  name: 'Interplanetary budget ' + uuid(),
+  amount: {
+    microAmount: '50000000'
+  },
+  deliveryMethod: 'STANDARD'
+};
+
+const budget_operations = {
+  operator: 'ADD',
+  operand: budget
+};
+
+// Add the budget.
+let budget_id = budget_service.mutate(
+  { operations: [budget_operations] },
+  (error, result) => {
+    console.log('budgetService', error, result);
+  }
+);
+console.log(
+  moment()
+    .add(1, 'month')
+    .format('YYYY-MM-DD')
+);
+//Construct operations and add campaigns.
+let operations = [
+  {
+    operator: 'ADD',
+    operand: {
+      name: 'Interplanetary Cruise ' + uuid(),
+      // Recommendation: Set the campaign to PAUSED when creating it to
+      // stop the ads from immediately serving. Set to ENABLED once you've
+      // added targeting and the ads are ready to serve.
+      status: 'PAUSED',
+      advertisingChannelType: 'SEARCH',
+      biddingStrategyConfiguration: {
+        biddingStrategyType: 'MANUAL_CPC'
       },
-      'deliveryMethod': 'STANDARD'
+      endDate: moment()
+        .add(1, 'month')
+        .format('YYYYMMDD'), //(datetime.datetime.now() +
+      //            datetime.timedelta(365)).strftime('%Y%m%d'),
+      // Note that only the budgetId is required
+      budget: {
+        budgetId: budget_id
+      },
+      networkSetting: {
+        targetGoogleSearch: 'true',
+        targetSearchNetwork: 'true',
+        targetContentNetwork: 'false',
+        targetPartnerSearchNetwork: 'false'
+      },
+      // Optional fields
+      startDate: moment()
+        .add(1, 'day')
+        .format('YYYYMMDD'), //(datetime.datetime.now() +
+      //              datetime.timedelta(1)).strftime('%Y%m%d'),
+      frequencyCap: {
+        impressions: '5',
+        timeUnit: 'DAY',
+        level: 'ADGROUP'
+      },
+      settings: [
+        {
+          xsi_type: 'GeoTargetTypeSetting',
+          positiveGeoTargetType: 'DONT_CARE',
+          negativeGeoTargetType: 'DONT_CARE'
+        }
+      ]
+    }
+  },
+  {
+    operator: 'ADD',
+    operand: {
+      name: 'Interplanetary Cruise banner  ' + uuid(),
+      status: 'PAUSED',
+      biddingStrategyConfiguration: {
+        biddingStrategyType: 'MANUAL_CPC'
+      },
+      endDate: moment()
+        .add(1, 'month')
+        .format('YYYYMMDD'), //(datetime.datetime.now() +
+      //           datetime.timedelta(365)).strftime('%Y%m%d'),
+      // Note that only the budgetId is required
+      budget: {
+        budgetId: budget_id
+      },
+      advertisingChannelType: 'DISPLAY'
+    }
   }
+];
 
-let budget_operations = {
-      'operator': 'ADD',
-      'operand': budget
-  }
+//let campaigns = campaign_service.mutate(operations)
 
-  // Add the budget.
-let budget_id = budget_service.mutate({operations: [budget_operations]}, (error, result) => {
-        console.log(error, result);
-    })
-console.log(budget_id);
-
-//   // Construct operations and add campaigns.
-// let operations = [{
-//       'operator': 'ADD',
-//       'operand': {
-//           'name': 'Interplanetary Cruise 12345' ,//% uuid.uuid4(),
-//           // Recommendation: Set the campaign to PAUSED when creating it to
-//           // stop the ads from immediately serving. Set to ENABLED once you've
-//           // added targeting and the ads are ready to serve.
-//           'status': 'PAUSED',
-//           'advertisingChannelType': 'SEARCH',
-//           'biddingStrategyConfiguration': {
-//               'biddingStrategyType': 'MANUAL_CPC',
-//           },
-//          // 'endDate': (datetime.datetime.now() +
-//           //            datetime.timedelta(365)).strftime('%Y%m%d'),
-//           // Note that only the budgetId is required
-//           'budget': {
-//               'budgetId': budget_id
-//           },
-//           'networkSetting': {
-//               'targetGoogleSearch': 'true',
-//               'targetSearchNetwork': 'true',
-//               'targetContentNetwork': 'false',
-//               'targetPartnerSearchNetwork': 'false'
-//           },
-//           // Optional fields
-//          // 'startDate': (datetime.datetime.now() +
-//           //              datetime.timedelta(1)).strftime('%Y%m%d'),
-//           'frequencyCap': {
-//               'impressions': '5',
-//               'timeUnit': 'DAY',
-//               'level': 'ADGROUP'
-//           },
-//           'settings': [
-//               {
-//                   'xsi_type': 'GeoTargetTypeSetting',
-//                   'positiveGeoTargetType': 'DONT_CARE',
-//                   'negativeGeoTargetType': 'DONT_CARE'
-//               }
-//           ]
-//       }
-//   }, {
-//       'operator': 'ADD',
-//       'operand': {
-//           'name': 'Interplanetary Cruise banner 23456',// % uuid.uuid4(),
-//           'status': 'PAUSED',
-//           'biddingStrategyConfiguration': {
-//               'biddingStrategyType': 'MANUAL_CPC'
-//           },
-//          // 'endDate': (datetime.datetime.now() +
-//            //           datetime.timedelta(365)).strftime('%Y%m%d'),
-//           // Note that only the budgetId is required
-//           'budget': {
-//               'budgetId': budget_id
-//           },
-//           'advertisingChannelType': 'DISPLAY'
-//       }
-//   }]
-//  //let campaigns = campaign_service.mutate(operations)
-
-// campaignService.mutate(operations, (error, result) => {
-//     console.log(error, result);
-// })
+campaignService.mutate({ operations: operations }, (error, result) => {
+  console.log('campaignService', error.body, result.body);
+});
