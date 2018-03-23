88ceda5afcbe7b119f7ae5881a6388722c08dbfe
Marek <marekz@gmail.com>
2018-03-23 10:23:50 -0400

    Fix order of fields

--- addCampaign.js
+++ addCampaign.js
@@ -31,12 +31,13 @@ const budget_operations = {
 };
 
 // Add the budget.
-let budget_id = budget_service.mutate(
-  { operations: [budget_operations] },
-  (error, result) => {
-    console.log('budgetService', error, result);
-  }
-);
+// let budget_id = budget_service.mutate(
+//   { operations: [budget_operations] },
+//   (error, result) => {
+//     console.log('budgetService', error, result);
+//   }
+// );
+
 console.log(
   moment()
     .add(1, 'month')
@@ -52,62 +53,68 @@ let operations = [
       // stop the ads from immediately serving. Set to ENABLED once you've
       // added targeting and the ads are ready to serve.
       status: 'PAUSED',
-      advertisingChannelType: 'SEARCH',
-      biddingStrategyConfiguration: {
-        biddingStrategyType: 'MANUAL_CPC'
-      },
-      endDate: moment()
-        .add(1, 'month')
-        .format('YYYYMMDD'), //(datetime.datetime.now() +
-      //            datetime.timedelta(365)).strftime('%Y%m%d'),
-      // Note that only the budgetId is required
+      startDate: '2018-03-01',
+      endDate: '2018-04-01',
       budget: {
-        budgetId: budget_id
+        budgetId: '1385733559'
       },
+      frequencyCap: {
+        impressions: '5',
+        timeUnit: 'DAY',
+        level: 'ADGROUP'
+      },
+      'settings': [
+        {
+          'xsi:type': 'GeoTargetTypeSetting',
+          'positiveGeoTargetType': 'DONT_CARE',
+          'negativeGeoTargetType': 'DONT_CARE'
+        }
+      ],
+      advertisingChannelType: 'SEARCH',
+
       networkSetting: {
         targetGoogleSearch: 'true',
         targetSearchNetwork: 'true',
         targetContentNetwork: 'false',
         targetPartnerSearchNetwork: 'false'
       },
-      // Optional fields
-      startDate: moment()
-        .add(1, 'day')
-        .format('YYYYMMDD'), //(datetime.datetime.now() +
-      //              datetime.timedelta(1)).strftime('%Y%m%d'),
-      frequencyCap: {
-        impressions: '5',
-        timeUnit: 'DAY',
-        level: 'ADGROUP'
-      },
-      settings: [
-        {
-          xsi_type: 'GeoTargetTypeSetting',
-          positiveGeoTargetType: 'DONT_CARE',
-          negativeGeoTargetType: 'DONT_CARE'
-        }
-      ]
-    }
-  },
-  {
-    operator: 'ADD',
-    operand: {
-      name: 'Interplanetary Cruise banner  ' + uuid(),
-      status: 'PAUSED',
       biddingStrategyConfiguration: {
         biddingStrategyType: 'MANUAL_CPC'
       },
-      endDate: moment()
-        .add(1, 'month')
-        .format('YYYYMMDD'), //(datetime.datetime.now() +
-      //           datetime.timedelta(365)).strftime('%Y%m%d'),
+      // endDate: moment()
+      //   .add(1, 'month')
+      //   .format('YYYYMMDD'), //(datetime.datetime.now() +
+      //            datetime.timedelta(365)).strftime('%Y%m%d'),
       // Note that only the budgetId is required
-      budget: {
-        budgetId: budget_id
-      },
-      advertisingChannelType: 'DISPLAY'
+
+      // Optional fields
+
+      //   .add(1, 'day')
+      //   .format('YYYYMMDD'), //(datetime.datetime.now() +
+      //              datetime.timedelta(1)).strftime('%Y%m%d'),
+
+
     }
-  }
+  },
+  // {
+  //   operator: 'ADD',
+  //   operand: {
+  //     name: 'Interplanetary Cruise banner  ' + uuid(),
+  //     status: 'PAUSED',
+  //     biddingStrategyConfiguration: {
+  //       biddingStrategyType: 'MANUAL_CPC'
+  //     },
+  //     endDate: moment()
+  //       .add(1, 'month')
+  //       .format('YYYYMMDD'), //(datetime.datetime.now() +
+  //     //           datetime.timedelta(365)).strftime('%Y%m%d'),
+  //     // Note that only the budgetId is required
+  //     budget: {
+  //       budgetId: ''
+  //     },
+  //     advertisingChannelType: 'DISPLAY'
+  //   }
+  // }
 ];
 
 //let campaigns = campaign_service.mutate(operations)
