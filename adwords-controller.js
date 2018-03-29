ca5da98c90a9965b23c0465ab7b92381b78b17b1
aengushearne <aengus.hearne@gmail.com>
2018-03-29 12:50:45 -0400

    Add API calls to class

--- adwords-controller.js
+++ adwords-controller.js
@@ -1,6 +1,9 @@
+// Operations must follow the order specified in the Adwords API docs. For example: https://developers.google.com/adwords/api/docs/reference/v201802/AdGroupService.AdGroup
+
 require('dotenv').load();
 const Promise = require('bluebird');
 const AdwordsUser = require('node-adwords').AdwordsUser;
+const AdwordsConstants = require('node-adwords').AdwordsConstants;
 
 module.exports = class AdwordsController {
   constructor() {
@@ -14,13 +17,15 @@ module.exports = class AdwordsController {
     });
   }
 
+  // *** Create requests
+
   /**
-   * 
-   * @param {object} options 
+   *
+   * @param {object} options
    * @param {String} options.name
    * @param {String} options.startDate
    * @param {String} options.endDate
-   * 
+   *
    */
 
   addCampaign(options) {
@@ -38,18 +43,18 @@ module.exports = class AdwordsController {
           startDate: options.startDate,
           endDate: options.endDate,
           budget: {
-            budgetId: options.budgetId,
+            budgetId: options.budgetId
           },
           frequencyCap: {
             impressions: '5',
             timeUnit: 'DAY',
             level: 'ADGROUP'
           },
-          'settings': [
+          settings: [
             {
               'xsi:type': 'GeoTargetTypeSetting',
-              'positiveGeoTargetType': 'DONT_CARE',
-              'negativeGeoTargetType': 'DONT_CARE'
+              positiveGeoTargetType: 'DONT_CARE',
+              negativeGeoTargetType: 'DONT_CARE'
             }
           ],
           advertisingChannelType: 'SEARCH',
@@ -67,7 +72,9 @@ module.exports = class AdwordsController {
       }
     ];
 
-    return Promise.fromCallback(callback => { campaignService.mutate({ operations: operations }, callback) })
+    return Promise.fromCallback(callback => {
+      campaignService.mutate({ operations: operations }, callback);
+    });
   }
 
   /**
@@ -77,7 +84,7 @@ module.exports = class AdwordsController {
    */
 
   addBudget(options) {
-    const budget_service = user.getService('BudgetService', 'v201802');
+    const budget_service = this.user.getService('BudgetService', 'v201802');
 
     // Create a budget, which can be shared by multiple campaigns.
     const budget = {
@@ -88,15 +95,185 @@ module.exports = class AdwordsController {
       deliveryMethod: 'STANDARD'
     };
 
-    const budget_operations = [{
-      operator: 'ADD',
-      operand: budget
-    }];
+    const budget_operations = [
+      {
+        operator: 'ADD',
+        operand: budget
+      }
+    ];
 
     // Add the budget.
     return Promise.fromCallback(callback => {
-      budget_service.mutate(
-        { operations: budget_operations }, callback)
-    })
+      budget_service.mutate({ operations: budget_operations }, callback);
+    });
   }
-}
+
+  /**
+   * @param {object} options
+   * @param {String} options.campaignId
+   * @param {String} options.name
+   */
+
+  addAdGroup(options) {
+    const ad_group_service = this.user.getService('AdGroupService', 'v201802');
+
+    const operations = [
+      {
+        operator: 'ADD',
+        operand: {
+          campaignId: options.campaignId,
+          name: options.name,
+          status: 'ENABLED',
+          settings: [
+            {
+              // # Targeting restriction settings. Depending on the
+              // # criterionTypeGroup value, most TargetingSettingDetail only
+              // # affect Display campaigns. However, the
+              // # USER_INTEREST_AND_LIST value works for RLSA campaigns -
+              // # Search campaigns targeting using a remarketing list.
+              'xsi:type': 'TargetingSetting',
+              details: [
+                // # Restricting to serve ads that match your ad group
+                // # placements. This is equivalent to choosing
+                // # "Target and bid" in the UI.
+                {
+                  'xsi:type': 'TargetingSettingDetail',
+                  criterionTypeGroup: 'PLACEMENT',
+                  targetAll: 'false'
+                },
+                // # Using your ad group verticals only for bidding. This is
+                // # equivalent to choosing "Bid only" in the UI.
+                {
+                  'xsi:type': 'TargetingSettingDetail',
+                  criterionTypeGroup: 'VERTICAL',
+                  targetAll: 'true'
+                }
+              ]
+            }
+          ],
+          biddingStrategyConfiguration: {
+            bids: [
+              {
+                'xsi:type': 'CpcBid',
+                bid: {
+                  microAmount: '1000000'
+                }
+              }
+            ]
+          }
+        }
+      }
+    ];
+
+    return Promise.fromCallback(callback => {
+      ad_group_service.mutate({ operations: operations }, callback);
+    });
+  }
+
+  addKeyword(options) {
+    const ad_group_criterion_service = this.user.getService(
+      'AdGroupCriterionService',
+      'v201802'
+    );
+
+    //Construct keyword ad group criterion object.
+    const keyword1 = {
+      adGroupId: options.adGroupId,
+      criterion: {
+        'xsi:type': 'Keyword',
+        text: 'mars',
+        matchType: 'BROAD'
+      },
+      'xsi:type': 'BiddableAdGroupCriterion',
+      // These fields are optional.
+      userStatus: 'PAUSED',
+      finalUrls: {
+        urls: ['http://example.com/mars']
+      }
+    };
+
+    const keyword2 = {
+      'xsi:type': 'NegativeAdGroupCriterion',
+      adGroupId: options.adGroupId,
+      criterion: {
+        'xsi:type': 'Keyword',
+        text: 'pluto',
+        matchType: 'EXACT'
+      }
+    };
+
+    const operations = [
+      {
+        operator: 'ADD',
+        operand: keyword1
+      },
+      {
+        operator: 'ADD',
+        operand: keyword2
+      }
+    ];
+
+    return Promise.fromCallback(callback => {
+      ad_group_criterion_service.mutate({ operations: operations }, callback);
+    });
+  }
+
+  addExpandedTextAd(options) {
+    const ad_group_ad_service = this.user.getService(
+      'AdGroupAdService',
+      'v201802'
+    );
+
+    const operations = [
+      {
+        operator: 'ADD',
+        operand: {
+          'xsi:type': 'AdGroupAd',
+          adGroupId: options.adGroupId,
+          ad: {
+            'xsi:type': 'ExpandedTextAd',
+            finalUrls: ['http://www.example.com/khkjhkjh'],
+            headlinePart1: 'Node test',
+            headlinePart2: 'node controller',
+            description: 'Buy your tickets now!'
+          },
+          // Optional fields.
+          status: 'PAUSED'
+        }
+      }
+    ];
+
+    return Promise.fromCallback(callback => {
+      ad_group_ad_service.mutate({ operations: operations }, callback);
+    });
+  }
+
+  // *** Read requests
+  getCampaigns(options) {
+    const campaign_service = this.user.getService('CampaignService', 'v201802');
+
+    //create selector
+    const selector = {
+      fields: ['Id', 'Name'],
+      ordering: [{ field: 'Name', sortOrder: 'ASCENDING' }],
+      paging: {
+        startIndex: 0,
+        numberResults: AdwordsConstants.RECOMMENDED_PAGE_SIZE
+      }
+    };
+
+    return Promise.fromCallback(callback => {
+      campaign_service.get({ serviceSelector: selector }, callback);
+    });
+  }
+
+  // getAccountHeirarchy(options) {
+  //   const managed_customer_service = this.user.getService('ManagedCustomerService', 'v201802');
+
+  //   // Refer to python script to implement this
+
+  //   return Promise.fromCallback(callback => {
+  //     managed_customer_service.get({ serviceSelector: selector }, callback);
+  //   });
+  // }
+};
